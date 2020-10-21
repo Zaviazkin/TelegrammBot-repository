e5c25197f23707377c2ab616ebc5679ba8e4fadc
Roman <zavyazkin8@gmail.com>
2020-10-21 17:00:07 +0600

    nu davai

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const Markup = require('telegraf/markup')
 
 const { Keyboard } = require('telegram-keyboard')
 
-const bot = new Telegraf(process.env.BOT_TOKEN1);
+const bot = new Telegraf("1161475824:AAEefR64wLIqLK2zft_WNohsooFCLx_e2XY");
 
 const {inlineMessageRatingKeyboard} = require('./botButtonModule')
 
